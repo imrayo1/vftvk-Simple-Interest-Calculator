04c534dc9cee52a2b9c4f12350cd36e9d5aace51
imrayo1 <imrayo@gmail.com>
2021-05-17 21:53:24 +0000

    pichon

--- script.js
+++ script.js
@@ -7,13 +7,16 @@ var interest = principal * years * rate /100;
  var year = new Date().getFullYear()+parseInt(years);
 
     p = document.getElementById("principal").value;
-    function updateRate() 
+    console.log("year = " + year + ", interest = " + interest)
+  
+
+
+}
+ function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
- document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+ document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    console.log("change")
 
-
-}
-}
-        
+}     
