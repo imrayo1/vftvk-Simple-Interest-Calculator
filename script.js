592482d03dabb09eb2add941e9542c69f6e0fead
William AG <antoniogutie@wisc.edu>
2021-05-17 20:51:43 -0700

    Code Formatting, use Alt + Shift + F for formatting

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-function compute()
-{
-var principal = document.getElementById("principal").value;
-  var rate = document.getElementById("rate").value;
- var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
- var year = new Date().getFullYear()+parseInt(years);
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
 
     p = document.getElementById("principal").value;
     console.log("year = " + year + ", interest = " + interest)
-  
+
 
 
 }
- function updateRate() 
-{
+function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
- document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>"
     console.log("change")
 
-}     
+}
