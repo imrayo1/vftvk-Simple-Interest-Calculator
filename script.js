c48246c361a5e4b358f8296845e5b434f108f91b
imrayo1 <imrayo@gmail.com>
2021-05-24 22:25:42 +0000

    hi

--- script.js
+++ script.js
@@ -4,14 +4,29 @@ function compute() {
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear();
+    var verify =  false
+
+    if (principal <= 0)  {
+    verify = false
+    }
+    else {
+     verify = true   
+    }
+    
 
     interest = principal * years * rate / 100;
     year = new Date().getFullYear() + parseInt(years);
 
     p = document.getElementById("principal").value;
 
-    document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>"
-
+    
+ if (verify == true)  {
+     document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>"
+    }
+    else {
+       document.getElementById("result").innerHTML =  "Enter a positve number"
+    }
+    
 
 }
 function updateRate() {
